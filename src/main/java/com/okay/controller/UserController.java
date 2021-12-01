642bfa0628d9b455ea39857e3fd035ef3422387d
Ahmet Okay <ahmetokay@users.noreply.github.com>
2021-12-01 16:04:30 +0300

    Add files via upload

--- src/main/java/com/okay/controller/UserController.java
+++ src/main/java/com/okay/controller/UserController.java
@@ -4,6 +4,7 @@ import com.okay.model.UserDto;
 import com.okay.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("test")
     @GetMapping(value = "/get")
     public ResponseEntity<UserDto> test1(@Valid @RequestParam(name = "userId") long userId) {
         return new ResponseEntity<>(userService.get(userId), HttpStatus.OK);
---
+++ src/main/java/com/okay/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.okay.service.impl;
+
+import com.okay.entity.User;
+import com.okay.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        if (user != null) {
+            return null;
+        }
+
+        throw new UsernameNotFoundException(username);
+    }
+}
